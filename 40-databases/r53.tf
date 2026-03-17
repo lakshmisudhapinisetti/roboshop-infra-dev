1912e77d7142413bb4d73010096e27871aef029d
Lakshmi Sudha Pinisetti <lakshmisudhapinisetti@ibm.com>
2026-03-17 15:18:41 +0530

    done

--- 40-databases/r53.tf
+++ 40-databases/r53.tf
@@ -7,14 +7,14 @@ resource "aws_route53_record" "mongodb" {
   allow_overwrite = true
 }
 
-# resource "aws_route53_record" "redis" {
-#   zone_id = var.zone_id
-#   name    = "redis-${var.environment}.${var.domain_name}"
-#   type    = "A"
-#   ttl     = "1"
-#   records = [aws_instance.redis.private_ip]
-#   allow_overwrite = true
-# }
+resource "aws_route53_record" "redis" {
+  zone_id = var.zone_id
+  name    = "redis-${var.environment}.${var.domain_name}"
+  type    = "A"
+  ttl     = "1"
+  records = [aws_instance.redis.private_ip]
+  allow_overwrite = true
+}
 
 # resource "aws_route53_record" "mysql" {
 #   zone_id = var.zone_id
