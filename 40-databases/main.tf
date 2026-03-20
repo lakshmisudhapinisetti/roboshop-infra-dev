c7e42d3347e17fd7d39c38bc9df70a0dc83e3acc
Lakshmi Sudha Pinisetti <lakshmisudhapinisetti@ibm.com>
2026-03-20 14:47:47 +0530

    done

--- 40-databases/main.tf
+++ 40-databases/main.tf
@@ -115,15 +115,15 @@ resource "terraform_data" "mysql" {
   }
 }
 
-resource "aws_instance" "redis" {
+resource "aws_instance" "rabbitmq" {
   ami           = local.ami_id
   instance_type = "t3.micro"
   subnet_id = local.database_subnet_id
-  vpc_security_group_ids = [local.redis_sg_id]
+  vpc_security_group_ids = [local.rabbitmq_sg_id]
 
   tags = merge(
     {
-        Name = "${var.project}-${var.environment}-redis"
+        Name = "${var.project}-${var.environment}-rabbitmq"
     },
     local.common_tags
   )
