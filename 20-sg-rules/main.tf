9c9583c17442b5b35699d36d27750bbd6c2d545d
Lakshmi Sudha Pinisetti <lakshmisudhapinisetti@ibm.com>
2026-03-20 19:07:24 +0530

    done

--- 20-sg-rules/main.tf
+++ 20-sg-rules/main.tf
@@ -69,8 +69,8 @@ resource "aws_security_group_rule" "rabbitmq_bastion" {
 
 resource "aws_security_group_rule" "backend_alb_bastion" {
   type              = "ingress"
-  from_port         = 8080
-  to_port           = 8080
+  from_port         = 80
+  to_port           = 80
   protocol          = "tcp"
   # Where traffic is coming from
   source_security_group_id = local.bastion_sg_id
