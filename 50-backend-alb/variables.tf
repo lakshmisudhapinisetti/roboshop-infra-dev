3f37e66fbb113b3ba5b3c16552daaa702b8419ad
Lakshmi Sudha Pinisetti <lakshmisudhapinisetti@ibm.com>
2026-03-21 16:52:52 +0530

    done

---
+++ 50-backend-alb/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z09768221YMR5IL33TM75"
+}
+
+variable "domain_name" {
+    default = "luckyy.shop"
+}
