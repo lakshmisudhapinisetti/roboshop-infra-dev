aaaf697fa6be7d6131932a513b77489efa76ea66
Lakshmi Sudha Pinisetti <lakshmisudhapinisetti@ibm.com>
2026-03-21 18:56:16 +0530

    done

--- 60-catalogue/bootstrap.sh
+++ 60-catalogue/bootstrap.sh
@@ -9,4 +9,4 @@ git clone "https://github.com/lakshmisudhapinisetti/ansible-roboshop-roles-tf.gi
 
 cd ansible-roboshop-roles-tf
 git pull
-ansible-playbook -e component=$component -e env=$environment roboshop.yaml
+ansible-playbook -i -e component=$component -e env=$environment roboshop.yaml
