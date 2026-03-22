5840a251d39bdecb09c6547591f15d388bd9a309
Lakshmi Sudha Pinisetti <lakshmisudhapinisetti@ibm.com>
2026-03-22 12:31:44 +0530

    done

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -1,22 +1,22 @@
 resource "aws_lb" "backend_alb" {
-  name               = "${var.project}-${var.environment}"
+  name               = "${var.project}-${var.environment}" # roboshop-dev
   internal           = true
   load_balancer_type = "application"
   security_groups    = [local.backend_alb_sg_id]
   subnets            = local.private_subnet_ids
- # keeping it as false, just to delete using terraform while practice
+
+  # keeping it as false, just to delete using terraform while practice
   enable_deletion_protection = false
-  
 
   tags = merge(
     {
-    Name = "${var.project}-${var.environment}"
-     },
-  local.common_tags
+        Name = "${var.project}-${var.environment}"
+    },
+    local.common_tags
   )
 }
 
-resource "aws_lb_listener" "backend_alb" {
+resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.backend_alb.arn
   port              = "80"
   protocol          = "HTTP"
@@ -25,12 +25,13 @@ resource "aws_lb_listener" "backend_alb" {
     type = "fixed-response"
 
     fixed_response {
-      content_type = "text/plain"
-      message_body = "<h1>I am from HTTP </h1>"
+      content_type = "text/html"
+      message_body = "<h1>Hi, I am from HTTP Backend ALB</h1>"
       status_code  = "200"
     }
   }
 }
+
 resource "aws_route53_record" "www" {
   zone_id = var.zone_id
   name    = "*.backend-alb-${var.environment}.${var.domain_name}"
